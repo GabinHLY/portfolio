7827e6a313bc8cd92884d5431ba0f705972b3398
GabinHLY <gabinhly@gmail.com>
2025-02-23 11:59:00 +0100

    Correction slug 1

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -5,23 +5,26 @@
     </div>
 
     <div v-if="project" class="project-details">
-      <h1 class="project-title">{{ project.name }}</h1>
-      <div class="project-metadata">
+      <div class="header">
+        <h1 class="project-title">{{ project.name }}</h1>
+        <span class="project-subtitle">{{ project.subtitle }}</span>
         <span class="project-date">{{ project.date }}</span>
       </div>
-      
+
       <img 
         :src="project.image" 
         :alt="project.name" 
         class="project-image"
       />
-      
+
       <div class="project-content">
         <h2>À propos de ce projet</h2>
         <p class="project-description">{{ project.description }}</p>
       </div>
+
+      <button class="learn-more">LEARN MORE</button>
     </div>
-    
+
     <div v-else class="not-found">
       <h2>Projet indisponible</h2>
       <p>Désolé, nous n'avons pas pu trouver le projet que vous recherchez.</p>
@@ -33,65 +36,64 @@
 <script>
 import { useRoute, useRouter } from 'vue-router';
 import { ref, watchEffect } from 'vue';
+import valorantImage from '@/assets/images/valorant.jpg';
 
 export default {
   setup() {
     const route = useRoute();
     const router = useRouter();
     const project = ref(null);
-    
+
     const projects = [
       { 
-        name: "CLUTCH | PROJET PERSONNEL", 
+        name: "CLUTCH", 
         date: "2024 - MAINTENANT", 
+        subtitle : "PROJET PERSONNEL",
         slug: "clutch", 
-        image: new URL('@/assets/images/valorant.jpg', import.meta.url).href,
+        image: valorantImage,
         description: "CLUTCH est une application innovante dédiée aux passionnés d’e-sport, offrant une plateforme centralisée pour suivre les résultats, les calendriers et les lives des plus grandes compétitions. Conçu pour les amateurs et les parieurs, CLUTCH intègre un système de paris basé sur des jetons virtuels, permettant aux utilisateurs de tester leurs connaissances sans contraintes légales."
       },
       { 
-        name: "SPORT ADDICT | PROFESSIONAL PROJECT", 
-        date: "2024 - 2025", 
+        name: "SPORT ADDICT", 
+        date: "2024 - 2025",
+        subtitle : "PROJET PROFESSIONNEL", 
         slug: "sport-addict", 
-        image: new URL('@/assets/images/valorant.jpg', import.meta.url).href,
+        image: valorantImage,
         description: "SportAddict est un site vitrine conçu pour un magasin de vêtements de sport, mettant en avant ses produits sans fonctionnalité d’achat en ligne. Développé avec Nuxt, Directus et Tailwind CSS, il offre une navigation fluide et une présentation optimisée des articles, triés par marque et catégorie de sport. Ce site permet aux clients de découvrir l’offre du magasin et d’obtenir des informations essentielles sur les produits disponibles en boutique."
       },
       { 
-        name: "HORSCAPE | SCHOOL PROJECT", 
+        name: "HORSCAPE", 
         date: "2024 - NOW", 
+        subtitle : "PROJET ACADÉMIQUE",
         slug: "horscape", 
-        image: new URL('@/assets/images/valorant.jpg', import.meta.url).href,
+        image: valorantImage,
         description: "Horscape est une plateforme conçue pour la réservation de box pour chevaux, inspirée du modèle Airbnb. Elle permet aux propriétaires de chevaux de trouver et de réserver des hébergements adaptés à leurs besoins, tout en offrant aux propriétaires de box un moyen simple de gérer leurs disponibilités. Développée avec une approche centrée sur l’expérience utilisateur, Horscape facilite la mise en relation entre les passionnés d’équitation et les structures d’accueil, en intégrant des fonctionnalités intuitives pour une réservation rapide et sécurisée."
       },
       { 
-        name: "API_UNIVERSE | SCHOOL PROJECT", 
+        name: "API_UNIVERSE", 
         date: "2024 - 2025", 
+        subtitle : "PROJET ACADÉMIQUE",
         slug: "api-universe", 
-        image: new URL('@/assets/images/valorant.jpg', import.meta.url).href,
+        image: valorantImage,
         description: "API_Universe est un projet académique visant à créer une API connectée à un frontend, permettant aux utilisateurs d’imaginer et de construire leurs propres univers peuplés de personnages interactifs. Grâce à l’intelligence artificielle, l’application génère des visuels uniques pour les mondes et leurs habitants, tout en offrant une expérience immersive où les utilisateurs peuvent dialoguer avec leurs créations. Cette fusion entre storytelling, IA et génération d’images ouvre la voie à une nouvelle forme d’interaction narrative et créative."
       },
       { 
-        name: "STOCK | PERSONAL PROJECT", 
+        name: "STOCK", 
         date: "2024 - 2025", 
+        subtitle : "PROJET PERSONNEL",
         slug: "stock", 
-        image: new URL('@/assets/images/valorant.jpg', import.meta.url).href,
+        image: valorantImage,
         description: "STOCK est un outil de gestion de stocks développé avec React, Express et MySQL, conçu pour simplifier le suivi des produits d’un magasin. Il permet d’ajouter, modifier et supprimer des articles tout en affichant leur quantité disponible en temps réel. Grâce à une interface intuitive, les utilisateurs peuvent facilement gérer leur inventaire et éviter les ruptures de stock. Cet outil offre une solution efficace pour optimiser la gestion des produits et faciliter le suivi des entrées et sorties en magasin."
       },
       { 
-        name: "BREAKOUT | SCHOOL PROJECT", 
+        name: "BREAKOUT", 
         date: "2024", 
+        subtitle : "PROJET ACADÉMIQUE",
         slug: "breakout", 
-        image: new URL('@/assets/images/valorant.jpg', import.meta.url).href,
+        image: valorantImage,
         description: "Breakout est un projet académique réalisé en équipe de trois, consistant à recréer le célèbre jeu d’arcade Breakout. Fidèle au gameplay original, le projet met en œuvre une gestion précise des collisions, une physique dynamique et une progression de difficulté. Ce développement a permis d’explorer des concepts clés du game development, comme la gestion des animations, les interactions avec le joueur et l’optimisation des performances."
       },
-      
     ];
-
-    const getProjectType = (name) => {
-      if (name.includes('PERSONAL')) return 'Personal Project';
-      if (name.includes('PROFESSIONAL')) return 'Professional Project';
-      if (name.includes('SCHOOL')) return 'School Project';
-      return 'Project';
-    };
 
     const loadProject = () => {
       project.value = projects.find(p => p.slug === route.params.slug);
@@ -99,14 +101,10 @@ export default {
 
     watchEffect(loadProject);
 
-    return { 
-      project,
-      getProjectType
-    };
+    return { project };
   }
 };
 </script>
-
 <style scoped>
 .content {
   max-width: 1100px;
